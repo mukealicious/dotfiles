f46c862e6184bfe5a89d233078d5d52ccd13c64c
mukealicious <1085841+mukealicious@users.noreply.github.com>
2026-06-16 23:27:42 -0700

    fix(pi): route restored sessions by profile

--- pi/aliases.fish
+++ pi/aliases.fish
@@ -1,7 +1,41 @@
 # Pi coding agent aliases
 
 function pi --wraps pi
-    switch "$PI_DEFAULT_PROFILE"
+    set -l profile "$PI_DEFAULT_PROFILE"
+    set -l expect_session_path 0
+
+    # Herdr restores panes with commands like `pi --session ~/.pi/personal/...`.
+    # Route those back to the profile that owns the session before using the
+    # machine default.
+    for arg in $argv
+        if test "$expect_session_path" -eq 1
+            if string match -q "$HOME/.pi/personal/*" -- "$arg"
+                set profile personal
+                break
+            else if string match -q "$HOME/.pi/work/*" -- "$arg"
+                set profile work
+                break
+            end
+            set expect_session_path 0
+            continue
+        end
+
+        switch "$arg"
+            case --session
+                set expect_session_path 1
+            case '--session=*'
+                set -l session_path (string replace -- '--session=' '' "$arg")
+                if string match -q "$HOME/.pi/personal/*" -- "$session_path"
+                    set profile personal
+                    break
+                else if string match -q "$HOME/.pi/work/*" -- "$session_path"
+                    set profile work
+                    break
+                end
+        end
+    end
+
+    switch "$profile"
         case personal
             command pi-personal $argv
         case '*'
